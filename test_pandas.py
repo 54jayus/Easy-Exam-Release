daba5bbe2c9436973189b4c6db73bd6300824524
229544971 <229544971@qq.com>
2026-06-12 14:20:17 +0800

    chore: 删除无用文件和临时目录

--- test_pandas.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-import pandas as pd
-pandas_libs_dir = os.path.join(os.path.dirname(pd.__file__), 'pandas.libs')
-print('pandas_libs_dir:', pandas_libs_dir)
-print('exists:', os.path.isdir(pandas_libs_dir))
-if os.path.isdir(pandas_libs_dir):
-    print('files:', os.listdir(pandas_libs_dir))
